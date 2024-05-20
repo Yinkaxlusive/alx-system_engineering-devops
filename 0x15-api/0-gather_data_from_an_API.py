7f7fae794b7af55263111999183dad5f7a8f6b11
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-05-20 19:57:15 +0100

    update gatha data

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
-"""
-This module fetches and displays the TODO list progress for a given employee ID
-using a REST API.
-"""
-
+"""Returns information about his/her to-do list progress."""
 import requests
 import sys
 
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com/'
+    user = requests.get(url + 'users/{}'.format(sys.argv[1])).json()
+    todos = requests.get(url + 'todos', params={"userId": sys.argv[1]}).json()
 
-def fetch_employee_todo_progress(employee_id):
-    """Fetch and display the TODO list progress for a given employee ID"""
-    # Define the base URL for the API
-    base_url = "https://jsonplaceholder.typicode.com"
-
-    # Fetch the employee's details
-    employee_url = f"{base_url}/users/{employee_id}"
-    employee_response = requests.get(employee_url)
-    employee = employee_response.json()
-
-    # Fetch the employee's TODO list
-    todos_url = f"{base_url}/todos"
-
+    completed = [todo for todo in todos if todo.get("completed") is True]
+    print('Employee {} is done with tasks({}/{}):'.format(
+        user.get("name"), len(completed), len(todos)))
+    [print('\t {}'.format(c.get("title"))) for c in completed]
