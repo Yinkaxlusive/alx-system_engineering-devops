bace1da480a753d1b40bb5e1761d0ebc1171770c
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-05-21 03:35:56 +0100

    Update 2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,66 @@
 #!/usr/bin/python3
-
 """
-Python script that exports data in the JSON format.
-"""
+This script exports data in CSV format"""
 
-from requests import get
-from sys import argv
 import json
+import requests
+import sys
+
+
+def export_todos_to_json(employee_id):
+    """
+    Returns info about an employee's TODO list progress
+    """
+    id_endpoint = f"https://jsonplaceholder.typicode.com/users/{employee_id}"
+    todos_endpoint = f"{id_endpoint}/todos"
+
+    try:
+        # Get user's data
+        response = requests.get(id_endpoint, timeout=10)
+        user_data = response.json()
+        username = user_data.get('username')
+
+        # Get tasks data
+        response = requests.get(todos_endpoint, timeout=10)
+        todos = response.json()
+
+        # Create a dictionary to store data
+        todos_dict = {
+            employee_id: []
+        }
+
+        # Populate the dictionary
+        for todo in todos:
+            task_completed_status = todo.get('completed')
+            task_title = todo.get('title')
+            todos_dict[employee_id].append({
+                "task": task_title,
+                "completed": task_completed_status,
+                "username": username
+            })
+
+        # Export data to JSON file
+        json_filename = employee_id + ".json"
+        with open(json_filename, 'w', encoding='utf-8') as json_file:
+            json.dump(todos_dict, json_file)
+
+        print("Data has been exported to " + json_filename)
+
+    except json.JSONDecodeError as err:
+        print(f"Error decoding JSON: {err}")
+    except Exception:
+        print("Something went wrong.")
+
 
 if __name__ == "__main__":
-    response = get('https://jsonplaceholder.typicode.com/todos/')
-    data = response.json()
+    if len(sys.argv) != 2:
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-    row = []
-    response2 = get('https://jsonplaceholder.typicode.com/users')
-    data2 = response2.json()
+    # check if input is an integer
+    ID = sys.argv[1]
+    if not ID.isdigit():
+        print("Usage: python script.py <employee_id>")
+        sys.exit(1)
 
-    for i in data2:
-        if i['id'] == int(argv[1]):
-            u_name = i['username']
-            id_no = i['id']
-
-    row = []
-
-    for i in data:
-
-        new_dict = {}
-
-        if i['userId'] == int(argv[1]):
-            new_dict['username'] = u_name
-            new_dict['task'] = i['title']
-            new_dict['completed'] = i['completed']
-            row.append(new_dict)
-
-    final_dict = {}
-    final_dict[id_no] = row
-    json_obj = json.dumps(final_dict)
-
-    with open(argv[1] + ".json",  "w") as f:
-        f.write(json_obj)
+    export_todos_to_json(ID)
