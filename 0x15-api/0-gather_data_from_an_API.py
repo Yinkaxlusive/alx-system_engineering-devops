8893846fcab04deb3d37db77ac06b4ed8d051f79
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-05-21 03:41:12 +0100

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,46 +1,28 @@
+
 #!/usr/bin/python3
-""" Python script which uses a REST API, for a given employee ID,
-returns information about their TODO list progress. """
+'''
+Python script that returns information using REST API
+'''
+import requests
+from sys import argv
 
-import json
-import sys
-import urllib
-import urllib.request
-
-# The base API url for getting the employee object
-USER_API_URL = "https://jsonplaceholder.typicode.com/users/"
-
-# The base API url for getting all todo objects for an employee
-TODO_API_URL = "https://jsonplaceholder.typicode.com/todos?userId="
-
-# Employee ID passed as an argument to the script
-emp_id: str = sys.argv[1] if len(sys.argv) > 1 else ""
-
-# Get all the todos for a given employee ID
-if emp_id.isdigit():
-    try:
-        user_url = f"{USER_API_URL}{emp_id}"
-        todos_url = f"{TODO_API_URL}{emp_id}"
-
-        emp_response = urllib.request.urlopen(user_url)
-        todos_response = urllib.request.urlopen(todos_url)
-
-        emp_data = emp_response.read()
-        todos_data = todos_response.read()
-
-        employee = json.loads(emp_data)
-        todos = json.loads(todos_data)
-
-        name = employee.get("name")
-        done = len([todo for todo in todos if todo.get("completed")])
-        total = len(todos)
-
-        print(f"Employee {name} is done with tasks({done}/{total}):")
-        for todo in todos:
-            if todo.get("completed"):
-                print(f"\t {todo.get('title')}")
-
-    except urllib.error.URLError as err:
-        print(f"An error occurred: {err}")
-    except json.JSONDecodeError as err:
-        print(f"Error decoding JSON: {err}")
+if __name__ == "__main__":
+    if len(argv) > 1:
+        user = argv[1]
+        url = "https://jsonplaceholder.typicode.com/"
+        req = requests.get("{}users/{}".format(url, user))
+        name = req.json().get("name")
+        if name is not None:
+            jreq = requests.get(
+                "{}todos?userId={}".format(
+                    url, user)).json()
+            alltsk = len(jreq)
+            completedtsk = []
+            for t in jreq:
+                if t.get("completed") is True:
+                    completedtsk.append(t)
+            count = len(completedtsk)
+            print("Employee {} is done with tasks({}/{}):"
+                  .format(name, count, alltsk))
+            for title in completedtsk:
+                print("\t {}".format(title.get("title")))
