1736fd9abf1ac3ec9675af52b6496ffcceb8ebf2
Yinkaxlusive <141252881+Yinkaxlusive@users.noreply.github.com>
2024-06-04 18:36:33 +0100

    Update 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,31 +1,30 @@
-#!/usr/bin/env puppet
+# Setup New Ubuntu server with nginx
+# and add a custom HTTP header
 
-# Puppet manifest to add a custom HTTP header with Puppet
-# The custom HTTP header name is X-Served-By
-# The value of the custom HTTP header is the hostname of the server Nginx is running on
+exec { 'update system':
+        command => '/usr/bin/apt-get update',
+}
 
-# Install Nginx package
 package { 'nginx':
-  ensure => installed,
+	ensure => 'installed',
+	require => Exec['update system']
 }
 
-# Define custom HTTP header configuration
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "# Custom HTTP header configuration\nserver {\n  listen 80 default_server;\n  listen [::]:80 default_server;\n  server_name _;\n\n  location / {\n    add_header X-Served-By $::hostname;\n    # Additional configuration\n  }\n}\n",
+file {'/var/www/html/index.html':
+	content => 'Hello World!'
 }
 
-# Enable the custom configuration
-file { '/etc/nginx/sites-enabled/default':
-  ensure => link,
-  target => '/etc/nginx/sites-available/default',
-  notify => Service['nginx'],
+exec {'redirect_me':
+	command => 'sed -i "24i\	rewrite ^/redirect_me https://th3-gr00t.tk/ permanent;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
-# Restart Nginx service to apply changes
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['/etc/nginx/sites-enabled/default'],
+exec {'HTTP header':
+	command => 'sed -i "25i\	add_header X-Served-By \$hostname;" /etc/nginx/sites-available/default',
+	provider => 'shell'
 }
 
+service {'nginx':
+	ensure => running,
+	require => Package['nginx']
+}
