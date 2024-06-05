afbf393cfb63e10d4e7a7347537606d2bb89d0a7
Yinkaxlusive <97adeyinkaonatola@gmail.com>
2024-06-05 23:17:13 +0100

    update file

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -5,17 +5,19 @@
 
 import requests
 
+
 def top_ten(subreddit):
     """
-    Queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit.
-    If the subreddit is invalid, prints None.
+    Queries the Reddit API and prints the
+    titles of the first 10 hot posts listed for a given subreddit
+    If the subreddit is invalid, prints None
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'custom-agent'}
     params = {'limit': 10}
-    
     try:
-        response = requests.get(url, headers=headers, params=params, allow_redirects=False)
+        response = requests.get(url, headers=headers, params=params,
+                                allow_redirects=False)
         if response.status_code == 200:
             try:
                 data = response.json()
@@ -28,5 +30,3 @@ def top_ten(subreddit):
             print(None)
     except requests.RequestException:
         print(None)
-
-
